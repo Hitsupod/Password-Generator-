90e48cc4b19fd01a9cc01d9c450ea89a79de4dab
Reggie Littleton <Hitsupod@gmail.com>
2020-05-02 17:55:31 -0500

    Test

--- styles/Javascript/Script.js
+++ styles/Javascript/Script.js
@@ -18,6 +18,17 @@ var characters = [
     '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '-', '?'
 ];
 
+var questions = [{
+    question: "How long would you like the password to be?"
+    amountLowestAns: 8;
+    amountHighestAns: 128;
+    toHigh: "To many characters"
+    toLow: "Not long Enough "
+}
+{
+    question: "Include Upper Case"
+    answer: "Yes"
+}]
 // Prompt for number of of character between > 8 and < 128 
 function amountPrompt() { 
     var fQuestion = prompt("How long would you like the password to be?")
@@ -38,41 +49,23 @@ function amountPrompt() {
 
     }
 }
+
+// Prompt for Upper Case
+function upperCase () {
+    var utxt;
+    var upper = confirm("Include Upper Case?")
+    if (upper == true) {
+        return upper;
+    } else (upper == false); {
+        alert("Weak but okay");
+    }
+}
+
 var el = document.getElementById("test");
 el.textContent = amountPrompt();
 // Make Prompts for Upper Lower And Special
 // Create Password
-function ranPassword () {
-    let complete = el
-    
-    let password = "";
 
-    // Lower Case Letters
-    let lowerletters = [
-        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-    ];
-    
-    // Upper Case Letters
-    let upperletters = [
-        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
-    ];
-    
-    // Number Choices 
-    let numbers = [
-        'ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+'
-        //'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '-', '?'
-    ];
-    
-    // Number Choices 
-    let characters = [
-        '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '-', '?'
-    ];
-
-    for (var i = 0; <= complete; i++) {
-        password = password + numbers.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
-    }
-}
-console.log(ranPassword());
 // var el = document.getElementById("test");
 // el.textContent = ranPassword();
 
