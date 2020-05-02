27fabe3f9c42e0814d700719f87b70fc8550e438
Reggie Littleton <Hitsupod@gmail.com>
2020-05-02 10:03:27 -0500

    More function updats

--- styles/Javascript/Script.js
+++ styles/Javascript/Script.js
@@ -1,6 +1,11 @@
-// Letter Choices
-var letters = [
+// Lower Case Letters 
+var lowerletters = [
 	'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+];
+
+// Upper Case Letters
+var upperletters = [
+	'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
 ];
 
 // Number Choices 
@@ -15,44 +20,62 @@ var characters = [
 
 // Prompt for number of of character between > 8 and < 128 
 function amountPrompt() { 
-    var length = prompt("How long would you like the password to be?")
-    length = parseInt(length);
-    if(isNaN(length)) {
+    var fQuestion = prompt("How long would you like the password to be?")
+    fQuestion = parseInt(fQuestion);
+    if(isNaN(fQuestion)) {
         alert("The input is not a number")
     }
     else {
-        if (length < 8) {
+        if (fQuestion < 8) {
             alert("Not long enough");
         }
-        else if (length >128) {
+        else if (fQuestion >128) {
             alert("To large");
         }
         else {
-            return length
+            return fQuestion
         }
 
     }
 }
 var el = document.getElementById("test");
 el.textContent = amountPrompt();
-// Function to create password
-function newpassword() {
-    var randomNumber = Math.floor(Math.random() * quotes.length);
-    document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
+// Make Prompts for Upper Lower And Special
+// Create Password
+function ranPassword () {
+    let complete = el
+    
+    let password = "";
+
+    // Lower Case Letters
+    let lowerletters = [
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+    ];
+    
+    // Upper Case Letters
+    let upperletters = [
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+    ];
+    
+    // Number Choices 
+    let numbers = [
+        'ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+'
+        //'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '-', '?'
+    ];
+    
+    // Number Choices 
+    let characters = [
+        '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '-', '?'
+    ];
+
+    for (var i = 0; <= complete; i++) {
+        password = password + numbers.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+    }
 }
+console.log(ranPassword());
+// var el = document.getElementById("test");
+// el.textContent = ranPassword();
 
-var myArray = ['yellow', 'gross', 'school', 'boy', 'work',  'love', 'no', 'broken', 'space', 'boring' ];
-
-var output = document.getElementById("result");
-var randGen = function () {
-  	
-  	output.innerHTML = "";
-  
-  	var rand = myArray[Math.round(Math.random() * (myArray.length - 1))];
-
-  	
-    output.innerHTML = rand;
-};
 
 
 
